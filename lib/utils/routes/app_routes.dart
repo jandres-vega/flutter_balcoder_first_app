3f780a856dcaeaf548415a99a42165125d2a751b
jandres-vega <jandresvega@itsa.edu.co>
2024-07-12 08:51:52 -0500

    Refactor imports widgets on routes

--- lib/utils/routes/app_routes.dart
+++ lib/utils/routes/app_routes.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_balcoder_first_app/ui/home/home_page.dart';
+import 'package:flutter_balcoder_first_app/ui/login/login_page.dart';
+import 'package:flutter_balcoder_first_app/ui/register/register_page.dart';
 
 class AppRoutes {
   static const String home = '/';
@@ -9,11 +12,11 @@ class AppRoutes {
   static Route<dynamic> generateRoute(RouteSettings settings){
     switch(settings.name){
       case home:
-        return MaterialPageRoute(builder: (context) => HomePage());
+        return MaterialPageRoute(builder: (context) => const HomePage());
       case login:
-        return MaterialPageRoute(builder: (context) => LoginPage());
+        return MaterialPageRoute(builder: (context) => const LoginPage());
       case register:
-        return MaterialPageRoute(builder: (context) => RegisterPage());
+        return MaterialPageRoute(builder: (context) => const RegisterPage());
       default:
         return CupertinoPageRoute(
           builder: (_) => Scaffold(
