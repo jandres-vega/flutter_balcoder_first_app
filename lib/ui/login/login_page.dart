2fa34c30b469fdaf1e3651115d9a91f859dd957b
jandres-vega <jandresvega@itsa.edu.co>
2024-07-13 10:05:48 -0500

    Feact: Add input to login

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_balcoder_first_app/utils/widget/custom_field_text.dart';
 import 'package:flutter_balcoder_first_app/utils/widget/main_scaffold.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,11 +16,25 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const MainScaffold(
+    return MainScaffold(
         title: 'Login',
         showDrawer: true,
         child: Center(
-          child: Text('Login Page'),
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            child: const Form(
+              child: Column(
+                children: [
+                  CustomFieldText(
+                      label: 'Email',
+                      password: false,
+                      hint: 'Ingresa el email',
+                      icon: Icons.confirmation_num_outlined
+                  ),
+                ],
+              ),
+            ),
+          )
         )
     );
   }
--- lib/utils/widget/main_scaffold.dart
+++ lib/utils/widget/main_scaffold.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_balcoder_first_app/utils/widget/drawer_widget.dart';
 
 class MainScaffold extends StatefulWidget {
-
   final Widget child;
   final String title;
   final bool showDrawer;
 
-  const MainScaffold({
-    super.key,
-    required this.child,
-    required this.title,
-    required this.showDrawer
-  });
+  const MainScaffold(
+      {super.key,
+      required this.child,
+      required this.title,
+      required this.showDrawer});
 
   @override
   State<StatefulWidget> createState() => _MainScaffoldState();
@@ -23,20 +21,26 @@ class _MainScaffoldState extends State<MainScaffold> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+          backgroundColor: Colors.deepOrangeAccent,
+          title: Text(widget.title),
           leading: widget.showDrawer
               ? Builder(
-              builder: (context) => IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              ))
+                  builder: (context) => IconButton(
+                        icon: const Icon(Icons.menu),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                      ))
               : IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.arrow_back))),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                icon: const Icon(Icons.home),
+                alignment: AlignmentDirectional.centerStart,
+                color: Colors.white,
+                padding: const EdgeInsets.all(8.0)
+          )
+      ),
       drawer: widget.showDrawer ? const Drawer(child: DrawerWidget()) : null,
       body: Padding(
         padding: const EdgeInsets.all(24.0),
