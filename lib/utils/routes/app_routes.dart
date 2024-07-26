4a124bcb671d85179fc73f3640efa67b9b43e87c
jandres-vega <jandresvega@itsa.edu.co>
2024-07-26 06:41:44 -0500

    feat: Add StorageService class for handling storage operations

--- lib/utils/routes/app_routes.dart
+++ lib/utils/routes/app_routes.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_balcoder_first_app/ui/auth/login/login_page.dart';
+import 'package:flutter_balcoder_first_app/ui/auth/register/register_page.dart';
 import 'package:flutter_balcoder_first_app/ui/home/home_page.dart';
-import 'package:flutter_balcoder_first_app/ui/login/login_page.dart';
-import 'package:flutter_balcoder_first_app/ui/register/register_page.dart';
+import 'package:flutter_balcoder_first_app/ui/splash/splash.dart';
+
 
 class AppRoutes {
-  static const String home = '/';
+  static const String splash = '/';
   static const String login = '/login';
   static const String register = '/register';
+  static const String home = '/home';
 
   static Route<dynamic> generateRoute(RouteSettings settings){
     switch(settings.name){
+      case splash:
+        return CupertinoPageRoute(builder: (context) => const Splash());
       case home:
         return MaterialPageRoute(builder: (context) => const HomePage());
       case login:
