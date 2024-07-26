6bc23599e6d561537d4afa9a5c582918b1a931ac
jandres-vega <jandresvega@itsa.edu.co>
2024-07-26 06:39:31 -0500

    Feact:Add images

---
+++ lib/utils/widget/blog_navigation.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_balcoder_first_app/ui/article/form_article.dart';
+import 'package:flutter_balcoder_first_app/ui/home/home_page.dart';
+import 'package:flutter_balcoder_first_app/ui/profile/profile.dart';
+
+class BlogNavigation extends StatefulWidget {
+  const BlogNavigation({super.key});
+
+  @override
+  State<BlogNavigation> createState() => _BlogNavigationState();
+}
+
+class _BlogNavigationState extends State<BlogNavigation> {
+
+  int indexTap = 0;
+
+  final List<Widget> widgetsChildren = [
+    const HomePage(),
+    const FormArticle(),
+    const ProfilePage()
+  ];
+
+  void onTapTapped(int index){
+    setState(() {
+      indexTap = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widgetsChildren[indexTap],
+      bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Colors.white,
+            primaryColor: Colors.purple,
+          ),
+          child: BottomNavigationBar(
+            currentIndex: indexTap,
+            onTap: onTapTapped,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Buscar",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "perfil",
+              )
+            ],
+          ),
+      )
+    );
+  }
+}
