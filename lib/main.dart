8e7da0fda5d68b15a8787355ec220549d779fb89
jandres-vega <jandresvega@itsa.edu.co>
2024-08-06 09:44:14 -0500

    chore: Update import paths in main.dart and inject dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_balcoder_first_app/app/inject_dependencies.dart';
 import 'app/my_app.dart';
 import 'firebase_options.dart';
 
@@ -8,21 +9,7 @@ main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  injectDependencies();
   runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'barcode first app',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       onGenerateRoute: AppRoutes.generateRoute,
-//     );
-//   }
-// }
