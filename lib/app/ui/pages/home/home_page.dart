fbd3d6d638c7c1d66e7a1d3e0c601045c0998d46
jandres-vega <jandresvega@itsa.edu.co>
2024-08-06 09:43:32 -0500

    feat: Add sign out functionality to home page

--- lib/app/ui/pages/home/home_page.dart
+++ lib/app/ui/pages/home/home_page.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_balcoder_first_app/app/domain/repository/authentication_repository.dart';
+import '../../../inject_dependencies.dart';
+import '../../../utils/widget/main_scaffold.dart';
 
-import '../../utils/widget/main_scaffold.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: MainScaffold(
           title: 'Home',
           showDrawer: true,
-          child: Text('Home Page')
-      )
+          child: ElevatedButton(
+              onPressed: ()async{
+                await getIt<AuthenticationRepository>().signOut();
+                Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+              },
+              child: const Text('Go to Register')
+          )
+      ),
     );
   }
 }
